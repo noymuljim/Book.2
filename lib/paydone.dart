a15f25ceb0dc7f33d1862b64de847c367c60e983
noymuljim <nm.jimnun@gmail.com>
2024-11-27 23:49:12 +0600

    appbar added in category page

---
+++ lib/paydone.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class paydone extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+ return Scaffold(
+   appBar: AppBar(),
+   body: Container(width: 400,height:400,
+   //child: Image.asset("assets/images/doen.")
+   ),
+ );
+  }
+
+}
